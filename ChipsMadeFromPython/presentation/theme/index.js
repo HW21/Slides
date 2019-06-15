3b9cabc76b712084fef2cb7599ec0cedafd82491
Dan Fritchman <daniel.fritchman@gmail.com>
2019-06-15 11:19:32 +0100

    Initial Spectacle/ MDX Combo

--- ChipsMadeFromPython/presentation/theme/index.js
+++ ChipsMadeFromPython/presentation/theme/index.js
@@ -1,16 +1,21 @@
 import createTheme from "../../createTheme";
 
+
 const colors = {
-  primary: "white",
-  secondary: "#1F2022", // almost black w/ blueish tint
-  tertiary: "#fc300f", // bright red
-  quaternary: "#CECECE" // pale gray
+  primary: 'pink',
+  secondary: '#1F2022',
+  tertiary: '#03A9FC',
+  quaternary: '#CECECE',
+};
+const fonts = {
+  primary: 'Montserrat',
+  secondary: 'Helvetica',
 };
 
-const theme = createTheme(colors, {
-    primary: "Montserrat",
-    secondary: "Helvetica"
-  }, {
+const theme = createTheme(
+  colors, 
+  fonts,
+  {
     progress: {
       pacmanTop: {
         background: colors.quaternary
@@ -23,35 +28,35 @@ const theme = createTheme(colors, {
       }
     },
     components: {
-      heading: {
-        h1: {
-          fontSize: '4rem',
-          textTransform: 'uppercase'
-        },
-        h2: {
-          fontSize: '3.5rem',
-          textTransform: 'uppercase'
-        },
-        h3: {
-          fontSize: '3rem',
-          textTransform: 'uppercase'
-        },
-        h4: {
-          fontSize: '2.5rem',
-          textTransform: 'uppercase'
-        },
-        h5: {
-          fontSize: '2rem',
-          textTransform: 'uppercase'
-        },
-        h6: {
-          fontSize: '1.5rem',
-          textTransform: 'uppercase'
-        }
-      },
+      // heading: {
+      //   h1: {
+      //     fontSize: '4rem',
+      //     textTransform: 'uppercase'
+      //   },
+      //   h2: {
+      //     fontSize: '3.5rem',
+      //     textTransform: 'uppercase'
+      //   },
+      //   h3: {
+      //     fontSize: '3rem',
+      //     textTransform: 'uppercase'
+      //   },
+      //   h4: {
+      //     fontSize: '2.5rem',
+      //     textTransform: 'uppercase'
+      //   },
+      //   h5: {
+      //     fontSize: '2rem',
+      //     textTransform: 'uppercase'
+      //   },
+      //   h6: {
+      //     fontSize: '1.5rem',
+      //     textTransform: 'uppercase'
+      //   }
+      // },
       codePane: {
-        fontSize: '2rem'
-      }
+        fontSize: '2.5rem'
+      },
     }
   });
 
