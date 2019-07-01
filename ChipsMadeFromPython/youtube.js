550efe16f67720c7029dc719a5bf8c7caf85a2f6
Dan Fritchman <daniel.fritchman@gmail.com>
2019-07-01 13:22:17 +0100

    Much More ChipsMadeFromPython

---
+++ ChipsMadeFromPython/youtube.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import YouTube from 'react-youtube';
+
+class YouTubeEmbed extends React.Component {
+    render() {
+        const { id } = this.props;
+        const opts = {
+            height: '600',
+            width: '800',
+            playerVars: {
+                // https://developers.google.com/youtube/player_parameters
+                autoplay: 1,
+                loop: 1,
+            }
+        };
+
+        return (
+            <YouTube
+                videoId={id}
+                opts={opts}
+            // onReady={this._onReady}
+            />
+        );
+    }
+
+    //   _onReady(event) {
+    //     // access to player in all event handlers via event.target
+    //     event.target.pauseVideo();
+    //   }
+}
+
+export default YouTubeEmbed;
