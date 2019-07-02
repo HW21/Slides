b3ac6677658d76f4799e1ea1d06ba2a8fe960582
Dan Fritchman <daniel.fritchman@gmail.com>
2019-07-02 17:24:49 +0100

    Base URL for GH-Pages

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 p=ChipsMadeFromPython
 
-mkdir -p docs/${p}
+mkdir -p docs/${p}/img
 cd $p
 yarn build 
 cd ..
 mv ${p}/dist/* ./docs/${p}
-cp -r ${p}/img ./docs/${p}/img
+cp -r ${p}/img/* ./docs/${p}/img/
 
 
